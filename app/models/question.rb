8d8cb24496f6453ba28db2ef77e14e6ec6d53404
Fabio L. Janiszevski <contato@fabiosammy.com>
2016-06-07 18:30:42 -0300

    Added quetion views with nested support

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
+  accepts_nested_attributes_for :answers, reject_if: lambda { |a| a[:answer].blank? }, allow_destroy: true
 end
